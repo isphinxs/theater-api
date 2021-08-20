5f588c29a5cc96ab62ba5f2a96855c57a0e2871b
Samantha Ostrowski <isphinxs@hotmail.com>
2021-08-20 14:02:32 -0400

    Draft Shows controller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,2 +1,56 @@
 class ShowsController < ApplicationController
+    before_action :set_show, only: [:show, :update, :destroy]
+    # GET /shows
+    def index
+        shows = Show.all
+        render json: shows, only: [:title, :theater, :director, :music, :lyrics, :book, :open_date]
+    end
+
+    # GET /shows/1
+    def show
+        if @show
+            render json: @show, only: [:title, :theater, :director, :music, :lyrics, :book, :open_date]
+        else
+            render json: { message: "Show not found. Please try again." }
+        end
+    end
+
+    # POST /shows
+    def create
+        show = Show.new(show_params)
+        if show.save
+            render json: show, status: :created
+        else
+            render json: { message: "Error creating show." }
+        end
+    end
+
+    # PATCH/PUT /shows/1
+    def update
+        if @show.update(show_paras)
+            render json: @show
+        else
+            render json: { message: "Error updating show." }
+        end
+    end
+
+    #DELETE /shows/1
+    def destroy
+        if @show.destroy
+            render json: { message: "Show successfully deleted." }
+        else
+            render json: { message: "Error deleting show." }
+        end
+    end
+
+    private
+
+    def set_show
+        @show = Show.find(params[:id])
+    end
+
+    def show_params
+        params.require(:show).permit(:title, :tehater, :director, :music, :lyrics, :book, :open_date)
+    end
+
 end
