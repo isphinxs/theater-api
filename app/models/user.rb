3c17c3929cbf7d05af749846e3f8a2bac4a769cc
Samantha Ostrowski <isphinxs@hotmail.com>
2021-08-25 23:00:08 -0400

    Update routes, add user validation, and draft JWT authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
     has_many :shows, through: :user_shows
 
     accepts_nested_attributes_for :shows
+
+    validates :email, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   resources :user_shows
   resources :shows
-  resources :users
+  # resources :users
+
+  get "/signup", to: "users#new"
+  post "/signup", to "users#create"
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  post "/logout", to: "sessions#destroy"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
