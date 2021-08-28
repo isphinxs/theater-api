a0b25bf96ad0fe9849a14ad35a29390ae1b1ca39
Samantha Ostrowski <isphinxs@hotmail.com>
2021-08-27 21:56:09 -0400

    Correct namespaced Auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources: :auth
+      resources :auth
     end
   end
-
-  # get "/signup", to: "users#new"
-  # post "/signup", to: "users#create"
-
-  # get "/login", to: "sessions#new"
-  # post "/login", to: "sessions#create"
-  # post "/logout", to: "sessions#destroy"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
