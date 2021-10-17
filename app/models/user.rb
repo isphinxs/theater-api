4f4601357f1dd7b947be7357b6d4cc97a0281139
Samantha Ostrowski <isphinxs@hotmail.com>
2021-10-17 17:34:23 -0400

    Add Ratings to User associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
     has_many :user_shows
     has_many :shows, through: :user_shows
     has_many :comments
+    has_many :ratings
 
     accepts_nested_attributes_for :shows
 
